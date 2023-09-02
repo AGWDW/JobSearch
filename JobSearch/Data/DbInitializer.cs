9349318d79f5292a5d9fa2b6b7f615e23d162d93
AlexanderGhosh <ghoshalexander@gmail.com>
2023-09-02 18:50:12 +0100

    Create seeded database if db isnt populated or in existance

---
+++ JobSearch/Data/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using JobSearch.Models;
+
+namespace JobSearch.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(JobSearchContext context)
+        {
+            if (context.JobSeekers.Any())
+            {
+                return;
+            }
+
+            var seekers = new JobSeeker[]
+            {
+                new JobSeeker{FirstName="Alexander", LastName="Ghosh"},
+                new JobSeeker{FirstName="Isabelle", LastName="Ghosh"}
+            };
+
+            context.JobSeekers.AddRange(seekers);
+            context.SaveChanges();
+        }
+    }
+}
--- JobSearch/Program.cs
+++ JobSearch/Program.cs
@@ -1,3 +1,5 @@
+using JobSearch.Data;
+using Microsoft.EntityFrameworkCore;
 namespace JobSearch
 {
     public class Program
@@ -5,9 +7,12 @@ namespace JobSearch
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<JobSearchContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("JobSearchContext") ?? throw new InvalidOperationException("Connection string 'JobSearchContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             var app = builder.Build();
 
@@ -17,6 +22,20 @@ namespace JobSearch
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<JobSearchContext>();
+                context.Database.EnsureCreated();
+                DbInitializer.Initialize(context);
             }
 
             app.UseHttpsRedirection();
