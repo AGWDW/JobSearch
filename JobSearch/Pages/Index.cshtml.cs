a56c89fe2fd6c0b6431bd63f75237c4b54c22ee5
AlexanderGhosh <ghoshalexander@gmail.com>
2023-09-03 13:16:26 +0100

    search bar apperance and value saving

--- JobSearch/Pages/Index.cshtml.cs
+++ JobSearch/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace JobSearch.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public string SearchString { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(string searchString)
         {
-
+            SearchString = searchString;
         }
     }
 }
---
+++ JobSearch/Pages/Shared/_JobListingPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobSearch.Pages.Shared
+{
+    public class _JobListingPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
