4e5f0fdb50469e64653ac9daa435ac4a05e93bc9
AlexanderGhosh <ghoshalexander@gmail.com>
2023-09-03 17:32:17 +0100

    apply button adds the job to userID == 1's applications and the user to the listing's aplicants

--- JobSearch/Pages/Index.cshtml.cs
+++ JobSearch/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using JobSearch.Data;
 using JobSearch.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,6 +29,26 @@ namespace JobSearch.Pages
                 IQueryable<JobListing> lisings = from j in _context.JobListings select j;
                 JobListings = await lisings.Where(j => j.Name.ToLower().Contains(SearchString.ToLower())).AsNoTracking().ToListAsync();
             }
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            const int userID = 1;
+
+            JobListing job = await _context.JobListings.Include(l => l.Applied).FirstOrDefaultAsync(j => j.ID == id);
+            JobSeeker me = await _context.JobSeekers.Include(j => j.JobsApplyedFor).FirstOrDefaultAsync(s => s.ID == userID);
+
+            var applications = (HashSet<JobListing>)me.JobsApplyedFor;
+            var applicants = (HashSet<JobSeeker>)job.Applied;
+
+            if (!applications.Contains(job) && !applicants.Contains(me))
+            {
+                applications.Add(job);
+                applicants.Add(me);
+                _context.SaveChanges();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
